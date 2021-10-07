0f70a3df14a4541da158b3291765cd6cf6e19cbf
FrBarbarossa <91803857+FrBarbarossa@users.noreply.github.com>
2021-10-07 23:11:38 +0300

    New Knowledge!
    Learnd ho to make cross-tables, slices in datasets. Learned how to apply functions to dataset. Now I know how to group rows/columns

--- ODS1 Hello world.py
+++ ODS1 Hello world.py
@@ -3,6 +3,10 @@ import pandas as pd
 
 pd.set_option('display.precision', 2)
 df = pd.read_csv('BigML_Dataset_1.csv')
+
+# pd.set_option("display.max_columns", None)
+# ^ можно убрать ограничение на вывод строк/столбцов
+
 print(df.head())  # Печать первых пяти строчек
 
 print(df.shape)  # Вывод данных о размерности таблицы
@@ -39,3 +43,33 @@ print(
 
 '''''Объединение критериев отбора и обработка кнкретного параметра'''
 print(df[(df["churn"] == 0) & (df["international plan"] == "no")]["total intl minutes"].max())
+
+print(df.loc[0:5, "state":"area code"])  # Вывод среза, первые 5 строк, столбцы от state до area code
+
+print(df.iloc[0:7, 1:4])  # вывод среза только по индексам: строки/столбцы
+
+'''Если нужна первая или последняя строка фрейма, то разрешено использовать:'''
+print(df[-1:])
+print(df[:1])
+
+'''Применить какую-либо функцию ко всем строкам фрейма:'''
+print(df.apply(np.max))
+
+'''Применяем лямба-функцию ко всем элементам столбца state c помощью lambda'''
+print(df[df["state"].apply(lambda state: state[0] == "W")].head())
+
+'''Группируем по значениям определённого столбца, выводим основную стат.информацию об интересующих столбцах (columns_to_show)'''
+columns_to_show = ["state", "account length", "customer service calls"]
+print(df.groupby(["state"])[columns_to_show].describe())
+
+'''Выводим отдельные данные (применяя функции numpy), группируя по столбцу'''
+print(df.groupby(['state'])[columns_to_show].agg([np.max, np.min]))
+
+'''Построение таблицы для сравнения значений по двум столбцам (парамертрам)'''
+print(pd.crosstab(df["state"], df["churn"], normalize=True))
+
+'''Составим сводную таблицу по заданным параметрам (ищем минимумы и максимумы), группируя по state'''
+print(df.pivot_table(
+    ["total intl minutes", "total day charge", "total night minutes"],
+    ["state"],
+    aggfunc=["min", "max"]))
